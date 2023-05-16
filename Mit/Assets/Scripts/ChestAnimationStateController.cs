4e9e8e3a4819b7f35ee6fda9b3dccecaf53ac393
Rasmus Schapiro <rasmus.schapiro@gmail.com>
2023-05-16 12:01:57 +0200

    added script and animator for chest piece

---
+++ Mit/Assets/Scripts/ChestAnimationStateController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestAnimationStateController : MonoBehaviour
+{
+    public Animator animator;
+    bool LeftHand;
+    bool RightHand;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // if right or left controller enters model's collision
+        if (other.gameObject.tag == "LeftHand")
+        {
+            // then change animation state
+            animator.SetBool("isMassaging", true);
+        }
+        if (other.gameObject.tag == "RightHand")
+        {
+            animator.SetBool("isMassaging", false);
+        }
+
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.gameObject.tag == "LeftHand" && other.gameObject.tag == "RightHand")
+    //    {
+    //        animator.SetBool("isMassaging", false);
+    //    }
+    //}
+}
